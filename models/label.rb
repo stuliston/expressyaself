15a07a004e8dbc70c360f80e3a39b8687dc23f26
Stuart Liston <stuart.liston@gmail.com>
2013-02-28 16:20:14 +1100

    Added gravatar to message presenter

--- models/label.rb
+++ models/label.rb
@@ -1,5 +1,15 @@
 class Label
 
+  class << self
+
+    def from_subject(subject)
+      subject.split(' ').collect do |label|
+        self.new(label)
+      end
+    end
+
+  end
+
   def initialize(label)
     @label = label
   end
--- spec/models/label_spec.rb
+++ spec/models/label_spec.rb
@@ -11,5 +11,16 @@ describe Label do
     it 'removes whitespace' do
       Label.new('awesome stuff yo').to_s.should == 'awesome-stuff-yo'
     end
+
+    it 'removes a leading hash' do
+      Label.new('#awesome-stuff-yo').to_s.should == 'awesome-stuff-yo'
+    end
+  end
+
+  describe '#from_subject' do
+
+    it 'returns an array of labels for a subject line' do
+      Label.from()
+    end
   end
 end
