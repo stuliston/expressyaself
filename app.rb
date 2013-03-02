b64b0e26af9ce9be154540c61e0d3cd571eb0427
Stuart Liston <stuart.liston@gmail.com>
2013-03-02 23:03:02 +1100

    Added homepage

--- app.rb
+++ app.rb
@@ -12,7 +12,8 @@ module ExpressYaSelf
   class App < Sinatra::Base
 
     get '/' do
-      haml :home
+      labels = GmailClient.instance.labels.all.reject! { |label| !label.start_with?('#') }
+      haml :home, locals: { labels: labels }
     end
 
     get '/:tag' do
@@ -39,6 +40,7 @@ module ExpressYaSelf
         messages << EmailPresenter.new(message)
       end
 
+      messages.reverse!
       haml :index, locals: { tag: requested_tag, messages: messages }
     end
 
