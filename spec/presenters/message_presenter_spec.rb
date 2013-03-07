fcd67d6dd3a9fcb719090c082d6d2225e3f2cc6f
Stuart Liston <stuart.liston@gmail.com>
2013-03-07 13:42:50 +1100

    Renamed message presenter

--- spec/presenters/message_presenter_spec.rb
+++ spec/presenters/message_presenter_spec.rb
@@ -1,5 +1,5 @@
 require 'ostruct'
-require './presenters/email_presenter'
+require './presenters/message_presenter'
 
 module ExpressYaSelf
 
@@ -8,7 +8,6 @@ module ExpressYaSelf
     subject { MessagePresenter.new(message) }
 
     describe '#author_name' do
-
       let(:message)     { double(author_name: author_name) }
       let(:author_name) { 'John Appleseed' }
 
@@ -19,12 +18,34 @@ module ExpressYaSelf
     end
 
     describe '#author_email' do
-
       let(:message)       { double(author_email: author_email) }
       let(:author_email)  { 'john.appleseed@apple.com' }
 
       it 'has the correct email' do
         subject.author_email.should == author_email
+      end
+    end
+
+    describe '#body' do
+      let(:message) { double(body: body) }
+
+      context 'when the body is nil' do
+
+        let(:body)  { nil }
+
+        it 'is [blank]' do
+          subject.body.should == '[blank]'
+        end
+
+      end
+
+      context 'when the body has a value' do
+        let(:body)  { 'a value' }
+
+        it 'is a value' do
+          subject.body.should == 'a value'
+        end
+
       end
     end
 
