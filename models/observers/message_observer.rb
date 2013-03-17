995f2f9e5dc2f21713eb4204ce08c5b332023f9f
Callum Dryden <callumdryden@gmail.com>
2013-03-17 15:47:19 +0000

    making the requred faye urls dynamic

--- models/observers/message_observer.rb
+++ models/observers/message_observer.rb
@@ -1,7 +1,7 @@
 module ExpressYaself
   class MessageObserver < Mongoid::Observer
     def after_save(message)
-      client = Faye::Client.new('http://192.168.1.109:1337/faye/faye')
+      client = Faye::Client.new('http://localhost:1337/faye/faye')
       message.tags.each do |tag|
         client.publish('/messages/' + tag[1, tag.length], message.to_json)
       end
